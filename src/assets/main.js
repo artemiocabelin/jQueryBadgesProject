6468deda1c9146b1a2a1453fc085bec311ae8341
artemiocabelin <acabeli@Artemios-Air.socal.rr.com>
2017-01-06 00:42:42 -0800

    added jquery and ajax build tasks

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,31 @@
-$(function() {
+$(document).ready(function() {
+	$(function() {
 
-  // your code will go here
+	  // your code will go here
 
+	  $.ajax({
+	    url: 'https://www.codeschool.com/users/artcab.json',
+	    dataType: 'jsonp',
+	    success: function(response) {
+	      // handle response
+	      console.log('response', response.courses.completed);
+
+	      function addCourses (courses) {
+	      	courses.forEach(function(course) {
+	      		var $newDiv = $('<div />', { "class" : "course"});
+	      		var $newH3 = $('<h3 />', {"text" : course.title });
+	      		var $newImg = $('<img>', { "src" : course.badge});
+	      		var $newAnchor = $('<a />', {"href" : course.url, "target" : "_blank", "class" : "btn btn-primary", "text" : "See Course"});
+	      		$("#badges").append($newDiv);
+	      		$($newDiv).append($newH3);
+	      		$($newH3).after($newImg);
+	      		$($newImg).after($newAnchor);
+	      	});
+	      }
+
+	      addCourses (response.courses.completed);
+	    }
+	  });
+	});
 });
+	
